a945674a5de78f01b28532c24be289cbf7d58fe5
Tushar Desarda <tushardesarda@outlook.com>
2023-11-17 20:17:16 +0530

    initial_sturcutre_commit

---
+++ Scaler_Problem/Prefix_Sum/Range_Sum_Query.java
@@ -0,0 +1,12 @@
+package Scaler_Problem.Prefix_Sum;
+
+/*
+ * Problem Statement
+ You are given an integer array A of length N.
+You are also given a 2D integer array B with dimensions M x 2, where each row denotes a [L, R] query.
+For each query, you have to find the sum of all elements from L to R indices in A (0 - indexed).
+More formally, find A[L] + A[L + 1] + A[L + 2] +... + A[R - 1] + A[R] for each query.
+ */
+public class Range_Sum_Query {
+    
+}
