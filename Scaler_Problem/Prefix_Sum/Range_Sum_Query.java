db5a077c4a0fdd6a4f9867117a29bd870fcb94f5
Tushar Desarda <tushardesarda@outlook.com>
2023-11-17 20:20:00 +0530

    solution_added

--- Scaler_Problem/Prefix_Sum/Range_Sum_Query.java
+++ Scaler_Problem/Prefix_Sum/Range_Sum_Query.java
@@ -8,5 +8,30 @@ For each query, you have to find the sum of all elements from L to R indices in 
 More formally, find A[L] + A[L + 1] + A[L + 2] +... + A[R - 1] + A[R] for each query.
  */
 public class Range_Sum_Query {
-    
+    public long[] rangeSum(int[] A, int[][] B) {
+        int N = A.length;
+      int M = B.length;
+      long[] prefixSum = new long[N];
+      long[] result = new long[M];
+
+      // Calculate the prefix sum
+      prefixSum[0] = A[0];
+      for (int i = 1; i < N; i++) {
+          prefixSum[i] = prefixSum[i - 1] + A[i];
+      }
+
+      // Process each query
+      for (int i = 0; i < M; i++) {
+          int L = B[i][0];
+          int R = B[i][1];
+
+          if (L == 0) {
+              result[i] = prefixSum[R];
+          } else {
+              result[i] = prefixSum[R] - prefixSum[L - 1];
+          }
+      }
+
+      return result;
+  }
 }
